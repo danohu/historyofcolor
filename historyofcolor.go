1b1ef69eb5fe7689d320fc610f8058c214ed0908
Dan O'Huiginn <daniel@ohuiginn.net>
2015-04-05 16:40:35 +0200

    Add unit test and travis config file
    just the skeleton
    
    contributes to #3, though with no real tests

--- historyofcolor.go
+++ historyofcolor.go
@@ -1,4 +1,4 @@
-package main
+package historyofcolor
 
 import (
 	"fmt"
---
+++ historyofcolor_test.go
@@ -0,0 +1,13 @@
+package historyofcolor
+
+import (
+	//"github.com/bmizerany/assert"
+	"testing"
+)
+
+
+func TestSkelenton(t *testing.T) {
+	var testfilename string = "testdata/albertin_virgin_c.jpg"
+	decodeimage(testfilename)
+	//assert.Equal(true,true)
+}
